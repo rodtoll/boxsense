3d35b8772d3a458187ee2c0d3011dc7f4298b841
Rod Toll <rod_toll@hotmail.com>
2013-05-01 23:51:54 -0700

    Working now with database based config

--- main.py
+++ main.py
@@ -3,31 +3,29 @@ __author__ = 'Rod'
 import DataSink
 import Sensors
 import time
+import mysql.connector
 
+connection = mysql.connector.connect(user='', password='', host='127.0.0.1', database='test')
 
+cursor = connection.cursor()
+cursor.execute('SELECT * from sensors')
+sensors = [Sensors.sensor_create(sensor_type, id, config) for id, sensor_type, config in cursor]
+cursor.close()
 
-sqlfeedconfig_json = '{"database_name": "test", "table_name": "sensor_readings", "field_map": {"tempc": 1, "hum": 2, "movement": 3}}'
+cursor = connection.cursor()
+cursor.execute('SELECT * from feeds')
+feeds = [DataSink.datasink_create(feed_type, config) for id, feed_type, config in cursor]
+cursor.close()
 
-motion_sensor_json = '{"pin_id":18, "no_movement_timeout_s": 6, "result_name":"movement"}'
-
-sqlfeed = DataSink.datasink_create(1, sqlfeedconfig_json)
-feed = DataSink.datasink_create(2, config_json)
-feed_isy = DataSink.datasink_create(3, isyfeedconfig_json)
-
-sensor = Sensors.sensor_create(2, 1, sensor_json)
-sensor2 = Sensors.sensor_create(1, 2, motion_sensor_json)
 
 while True:
-    points = sensor.get_datapoints()
-    points.update(sensor2.get_datapoints())
 
-    for key, value in points.items():
-        print("Key:" +str(key) + "Value: " + str(value))
-
-    feed.upload_datapoints(sensor, points)
-    feed_isy.upload_datapoints(sensor, points)
-    sqlfeed.upload_datapoints(sensor, points)
+    # read out the current data points
+    for sensor in sensors:
+        points_to_upload = sensor.get_datapoints()
+        for feed in feeds:
+            print("Feed: " + str(type(feed)))
+            feed.upload_datapoints(sensor, points_to_upload)
 
     time.sleep(3)
-
 
